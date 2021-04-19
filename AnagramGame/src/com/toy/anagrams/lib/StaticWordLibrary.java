b57ed0390e4922cf672a43b26f515eac88c4e9c0
alumno <alumno@f1302>
2021-04-19 11:42:26 +0200

    prueba

--- AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -27,7 +27,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
-        "rcuello",
+        "bosco2",
         "abstraction",
         "ambiguous",
         "arithmetic",
