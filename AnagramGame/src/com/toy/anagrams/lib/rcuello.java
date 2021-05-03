0cdca7671864f66b3dd815136f105d0acf6d3027
alumno <alumno@b37prf>
2021-05-03 09:59:45 +0200

    Merge origin/rcuello into main
    Conflicts:
    	AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java

---
+++ AnagramGame/src/com/toy/anagrams/lib/rcuello.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.lib;
+
+/**
+ *
+ * @author alumno
+ */
+
+public class rcuello {
+  // esto es un comentario de rcuello 3/05/21 
+    
+    
+}
